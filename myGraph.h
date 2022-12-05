8851b6b2d7d4992e98013cdaad1d93dd986513cf
Gabriel Jerome <Gabriel.Jerome@mines.sdsmt.edu>
2022-12-04 22:37:26 -0700

    Revert "need to work on isBridge"
    This reverts commit 109712991ea74d0b29923f1a3a500ae1c417c099.

--- myGraph.h
+++ myGraph.h
@@ -35,7 +35,7 @@ bool isEmpty( vector<vector<int>> &g );
 
 bool isSource( vector<vector<int>> &g, int vertex );
 
-bool isBridge( vector<vector<int>> &g, edge curr );
+
 
 
 class myGraph
@@ -57,7 +57,7 @@ public:
     vector<int> BFS( int s, int t );
     myGraph kruskalsMST( );
     bool isCyclicUndirected( int v, vector<bool> visited, int last );
-    myGraph eulerCicuit( );
+
     friend ostream& operator<<( ostream& out, myGraph &g );
     friend bool operator==( const myGraph &l, const myGraph &r );
     friend void readDot( ifstream &fin, myGraph &g );
